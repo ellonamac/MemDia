6ba23707c1117b7b4676ef5f7041b17eefdca290
Chris Mayfield <mayfiecs@jmu.edu>
2024-04-19 14:24:28 -0400

    review code and fix some typos

--- memdia.js
+++ memdia.js
@@ -8,10 +8,9 @@ const BOX_WH = 40;  // min width/height of var box
 const BG_COLOR = "#fafafa";  // TODO use CSS instead
 const FG_COLOR = "#000000";  // TODO use CSS instead
 
-// const INLINE = [];
+// Buffer for creating strings and arrays
+const INLINE = [];
 
-
-//  QUESTIONNS : what is '__'
 
 function draw_rect(svg, box) {
     let rect = document.createElementNS(NS, "rect");
@@ -21,7 +20,9 @@ function draw_rect(svg, box) {
     rect.setAttribute("y", box.y + box.type_height);
     rect.setAttribute("width", box.width - box.name_width);
     rect.setAttribute("height", box.height - box.type_height - box.index_height);
-    rect.setAttribute("stroke", FG_COLOR);
+    if (!box.name.includes("__")) {
+        rect.setAttribute("stroke", FG_COLOR);
+    }
     rect.setAttribute("fill", "none");
 }
 
@@ -53,6 +54,7 @@ function draw_type(svg, box) {
 }
 
 function draw_value(svg, box) {
+    // TODO if not ARROWS or box.op != "@"
     let text = document.createElementNS(NS, "text");
     svg.appendChild(text);
     text.textContent = box.value;
@@ -73,21 +75,24 @@ function draw_index(svg, box) {
         text.setAttribute("x", box.x + box.width / 2 + box.name_width / 2);
         text.setAttribute("y", box.y + box.height - TEXT_H / 3);
         text.setAttribute("text-anchor", "middle");
-        text.setAttribute("fill", FG_COLOR);
-        text.setAttribute("font-stlye", "italic");
+        text.setAttribute("alignment-baseline", "middle");
+        text.setAttribute("font-style", "italic");
         text.setAttribute("font-size", "0.8em");
     }
 }
 
+
 class Diagram {
     constructor(code) {
+
+        // coordinates and dimensions
         this.left_y = MARGIN;
         this.left_width = 3 * MARGIN;
         this.right_y = this.left_y;
         this.right_width = this.left_width;
 
+        // parse functions and objects
         this.nodes = [];
-
         let blocks = code.trim().split("\n\n");
         for (const block of blocks) {
             let lines = block.split("\n");
@@ -105,27 +110,25 @@ class Diagram {
                 this.right_width = Math.max(this.right_width, 3 * MARGIN + node.width);
             }
 
-            // What is the purpose of INLINE
-            //     while (INLINE.length > 0){
-            //         lines = INLINE.shift().split();
-            //         let node = new LargeBox(lines, this.left_width + 2 * MARGIN, this.right_y);
-            //         this.nodes.push(node);
-            //         this.right_y += node.height + MARGIN;
-            //         this.right_width = Math.max(this.right_width, 3 * MARGIN + node.width);
-            //     }
+            while (INLINE.length > 0) {
+                // string or array (keep code in sync with else block)
+                lines = INLINE.shift().split();
+                let node = new LargeBox(lines, this.left_width + 2 * MARGIN, this.right_y);
+                this.nodes.push(node);
+                this.right_y += node.height + MARGIN;
+                this.right_width = Math.max(this.right_width, 3 * MARGIN + node.width);
+            }
         }
 
         // diagram size
         this.width = this.left_width + this.right_width;
         this.height = Math.max(this.left_y, this.right_y);
-
     }
     render(div) {
         let svg = document.createElementNS(NS, "svg");
-        svg.setAttribute("width", this.left_width + this.right_width);
-        svg.setAttribute("height", Math.max(this.left_y, this.right_y));
+        svg.setAttribute("width", this.width);
+        svg.setAttribute("height", this.height);
         svg.style.backgroundColor = BG_COLOR;
-
 
         // replace previous contents
         while (div.firstChild) {
@@ -142,12 +145,11 @@ class Diagram {
 
 class LargeBox {
     constructor(lines, x, y) {
-        this.nodes = [];
         this.x = x;
         this.y = y;
 
         if (lines[0].includes(":")) {
-            // parse the block specifiation
+            // parse the block specification
             let parts = lines[0].split(":");
             this.name = parts[0].trim();
             this.type = parts[1].trim();
@@ -158,7 +160,7 @@ class LargeBox {
         } else {
             // noname block (global variables)
             this.name = "";
-            this.type = ""; // no type = function
+            this.type = "";  // no type = function
 
             this.margin = 0;
             this.width = 0;
@@ -171,49 +173,54 @@ class LargeBox {
             this.height = MARGIN;
         }
 
-
         // estimate width and height
         if (this.name && this.height > 0) {
             this.name_width = (this.name.length + 1) * TEXT_W;
             this.width += this.name_width;
             x += this.name_width;
-        } else { this.name_width = 0; }
+        } else {
+            this.name_width = 0;
+        }
 
-        if (this.type != "") {
+        if (this.type) {
             this.type_height = TEXT_H;
             this.height += this.type_height
             y += this.type_height;
         }
-        else { this.type_height = 0; }
+        else {
+            this.type_height = 0;
+        }
 
         if (this.type.endsWith("[]")) {
             this.index_height = TEXT_H;
             this.height += this.index_height;
-        } else { this.index_height = 0; }
-
+        } else {
+            this.index_height = 0;
+        }
 
         // parse the remaining lines
+        this.nodes = [];
         let i = 0;
         if (lines[0].includes(":")) {
             i = 1;
         }
+
+        // parse the variables
         for (; i < lines.length; i++) {
             let node = new SmallBox(lines[i], x + this.margin, y + this.margin);
             this.nodes.push(node);
-            this.width = Math.max(this.width, 2 * this.margin + this.name_width +
-                node.width); // BECOMING NAN HERE (something about the name width)
+            this.width = Math.max(this.width, 2 * this.margin + this.name_width + node.width);
             this.height += node.height + MARGIN;
             y += node.height + MARGIN;
         }
     }
     render(svg) {
-        if (!this.name == "") {
+        if (this.name != "") {
             draw_rect(svg, this);
         }
         draw_name(svg, this);
         draw_type(svg, this);
         draw_value(svg, this);
-        // should call small boxes render
         for (let i = 0; i < this.nodes.length; i++) {
             this.nodes[i].render(svg);
         }
@@ -226,7 +233,6 @@ class SmallBox {
         this.y = y;
 
         let line = code.trim();
-
         if (line.startsWith('"') && line.endsWith('"')) {
             // String literal
             this.name = "";
@@ -240,7 +246,6 @@ class SmallBox {
             this.index_height = 0;
         } else if (line.startsWith('{') && line.endsWith('}')) {
             // Array literal
-            this.name = ""; // this does not need to be here
             this.op = '@';
             this.value = [];
             this.width = 0;
@@ -256,12 +261,10 @@ class SmallBox {
                 this.width += box.width;
                 this.value.push(box);
             }
-
         } else {
             // Variable box
-
             [this.type, this.name, this.op, ...this.value] = line.split(" ");
-            this.value = this.value.join(" "); // ADD TRIM HERE
+            this.value = this.value.join(" ");  // TODO add trim here?
 
             if (code.includes("__")) {
                 this.name_width = 0;
@@ -281,7 +284,6 @@ class SmallBox {
             }
             this.height = this.type_height + BOX_WH + this.index_height;
         }
-
     }
     render(svg) {
         draw_rect(svg, this);
